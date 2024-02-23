b163c7bdfa449db6a2cc6c687389218609b616fa
Vladimir Plyukhin <vladimirplyukhin89@gmail.com>
2024-02-23 12:47:33 +0500

    Fihished 03-navigating-jsx-types task

--- src/01-introduction/03-navigating-jsx-types.problem.tsx
+++ src/01-introduction/03-navigating-jsx-types.problem.tsx
@@ -1,10 +1,17 @@
+import React from 'react';
 
+interface IComponent extends React.HTMLAttributes<HTMLDivElement> {
+  'aria-posinset': number
+  'aria-setsize': number
+  onChange: React.FormEventHandler<HTMLDivElement>
+}
 
-export const Component = () => {
+export const Component: React.FC<IComponent> = () => {
   return (
     <div
       // How do I figure out what type aria-posinset expects?
       aria-posinset={1}
+      aria-setsize={1}
       // How do I figure out what type onChange expects?
       onChange={() => {}}
 
